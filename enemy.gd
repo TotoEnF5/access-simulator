a602c666c51cc8f9f7e76232659f733e70f24e58
TotoEnF5 <totooodev@proton.me>
2026-01-27 14:47:40 +0100

    Add enemy spawning / collisions

--- enemy.gd
+++ enemy.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 @export var speed: float = 100.0
-@export var direction: Vector2 = Vector2(1.0, 0.0)
+@export var direction: Vector2 = Vector2(-1.0, 0.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,10 +11,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
+	
+	
 func _physics_process(delta: float) -> void:
 	var collision = self.move_and_collide(self.direction * self.speed)
 	if collision:
-		collision.get_collider().free()
+		# collision.get_collider().free()
 		self.queue_free()
---
+++ shmup.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var enemyCooldownMs: int = 1000
+
+var _lastEnemyTime: int = 0
+
+const _enemyScene: PackedScene = preload("res://enemy.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var now: int = Time.get_ticks_msec()
+	if now > self.enemyCooldownMs + self._lastEnemyTime:
+		self._spawn_enemy()
+		self._lastEnemyTime = now
+		
+
+func _spawn_enemy() -> void:
+	var viewportSize = get_viewport().get_visible_rect().size
+	var y = randf() * viewportSize.y
+	var enemyPos = Vector2(viewportSize.x + 200, y)
+	var enemy = self._enemyScene.instantiate()
+	enemy.position = enemyPos
+	add_child(enemy)
+	
