c7472736d1bcad4263072f6f1fa660ff732694f6
TotoEnF5 <totooodev@proton.me>
2026-01-27 15:38:46 +0100

    Better platformer camera

--- platformer.gd
+++ platformer.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Camera2D.position = $Player.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Camera2D.position = $Player.position
+	$Camera2D.position = $Camera2D.position.lerp($Player.position, delta * 4.0)
