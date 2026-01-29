9af945124fc3f1e839c0e08374886bdbd7168143
Séverin <severin.bm.richard@gmail.com>
2026-01-29 13:55:16 +0100

    update: improve sound settings menu

--- ui/sound_slider.gd
+++ ui/sound_slider.gd
@@ -2,8 +2,21 @@ extends HSlider
 @export var rtpc:WwiseRTPC
 @export var rtpc_name:String = "None"
 
-func _ready() -> void:
-	$RichTextLabel.text = rtpc_name
+@export var event_feedback: WwiseEvent
 
-func _on_value_changed(value: float) -> void:
-	rtpc.set_value(self, value)
+func _enter_tree() -> void:
+	self.value = rtpc.get_global_value()
+	$RichTextLabel.text = rtpc_name + " " + str(round(value))
+	
+
+func _on_drag_ended(_value_changed: bool) -> void:
+	event_feedback.stop(self)
+
+
+func _on_drag_started() -> void:
+	event_feedback.post(self)
+
+
+func _on_value_changed(new_value: float) -> void:
+	rtpc.set_global_value(new_value)
+	$RichTextLabel.text = rtpc_name + " " + str(roundf(new_value))
