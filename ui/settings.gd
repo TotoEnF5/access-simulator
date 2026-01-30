b4ca90e60237fae64d25fdac074583be51951e48
Séverin <severin.bm.richard@gmail.com>
2026-01-30 12:07:49 +0100

    Add: UI sound for access menu

--- ui/settings.gd
+++ ui/settings.gd
@@ -13,51 +13,62 @@ func _process(delta: float) -> void:
 
 func _on_high_contrast_toggled(toggled_on: bool) -> void:
 	access_settings.set_high_contrast(toggled_on)
-
+	SoundbankManager.event_UI.post(self)
 
 func _on_player_speed_slider_value_changed(value: float) -> void:
 	access_settings.set_vroom_player_speed(value)
+	SoundbankManager.event_UI.post(self)
 
 
 func _on_player_rotation_speed_slider_value_changed(value: float) -> void:
 	access_settings.set_vroom_player_rotation_speed(value)
+	SoundbankManager.event_UI.post(self)
 
 
 func _on_shmup_player_speed_slider_value_changed(value: float) -> void:
 	access_settings.set_shmup_player_speed(value)
+	SoundbankManager.event_UI.post(self)
 
 
 func _on_shmup_player_projectile_speed_slider_value_changed(value: float) -> void:
 	access_settings.set_shmup_player_projectile_speed(value)
+	SoundbankManager.event_UI.post(self)
 
 
 func _on_player_projectile_cooldown_slider_value_changed(value: float) -> void:
 	access_settings.set_shmup_player_projectile_cooldown(value)
-
+	SoundbankManager.event_UI.post(self)
 
 func _on_shmup_enemy_speed_slider_value_changed(value: float) -> void:
 	access_settings.set_shmup_enemy_speed(value)
+	SoundbankManager.event_UI.post(self)
 
 
 func _on_shmup_enemy_projectile_speed_slider_value_changed(value: float) -> void:
 	access_settings.set_shmup_enemy_projectile_speed(value)
-
+	SoundbankManager.event_UI.post(self)
 
 func _on_shmup_enemy_projectile_cooldown_slider_value_changed(value: float) -> void:
 	access_settings.set_shmup_enemy_projectile_cooldown(value)
-
+	SoundbankManager.event_UI.post(self)
 
 func _on_shmup_enemy_frequency_slider_value_changed(value: float) -> void:
 	access_settings.set_shmup_enemy_cooldown(value)
-
+	SoundbankManager.event_UI.post(self)
 
 func _on_platformer_player_speed_slider_value_changed(value: float) -> void:
 	access_settings.set_platformer_player_speed(value)
 
-
 func _on_platformer_player_jump_force_slider_value_changed(value: float) -> void:
 	access_settings.set_platformer_player_jump(value)
 
-
 func _on_platformer_player_gravity_slider_value_changed(value: float) -> void:
 	access_settings.set_platformer_player_gravity(value)
+
+
+func _on_tab_container_tab_changed(tab: int) -> void:
+	SoundbankManager.event_UI.post(self)
+
+
+func _on_check_button_pressed() -> void:
+	SoundbankManager.event_UI.post(self)
